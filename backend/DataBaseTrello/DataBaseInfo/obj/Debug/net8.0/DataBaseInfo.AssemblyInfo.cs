7ba75167c3d0b8d503a34b75baf17cc86e145c0f
KCSSB <Nehbri.nehbri@yandex.ru>
2025-06-30 19:55:40 +0300

    InitialDockerComit

--- backend/DataBaseTrello/DataBaseInfo/obj/Debug/net8.0/DataBaseInfo.AssemblyInfo.cs
+++ backend/DataBaseTrello/DataBaseInfo/obj/Debug/net8.0/DataBaseInfo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataBaseInfo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+560b2395f936adeb5b30665beda73ce228ecaa10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dad34784a0c9180c08e743a1b25667d4e64f68e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataBaseInfo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataBaseInfo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
