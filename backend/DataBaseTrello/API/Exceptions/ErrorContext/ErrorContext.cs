7188f7ccc222012ba3b5af84053976180f8a698c
KCSSB <Nehbri.nehbri@yandex.ru>
2025-07-17 13:06:37 +0300

    UpdateErrorContext

--- backend/DataBaseTrello/API/Exceptions/ErrorContext/ErrorContext.cs
+++ backend/DataBaseTrello/API/Exceptions/ErrorContext/ErrorContext.cs
@@ -2,5 +2,13 @@
 {
     public class ErrorContext
     {
+        public string Service { get; set; } = string.Empty;
+        public string Operation { get; set; } = string.Empty;
+
+        public ErrorContext(string service, string operation)
+        {
+            Service = service;
+            Operation = operation;
+        }
     }
 }
