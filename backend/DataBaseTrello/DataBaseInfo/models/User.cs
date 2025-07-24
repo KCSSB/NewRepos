3c3fa4608fb2ac435030afa9d873d5eaa834884d
KCSSB <Nehbri.nehbri@yandex.ru>
2025-07-24 21:17:32 +0300

    ChangeEntityUserIsEnd

--- backend/DataBaseTrello/DataBaseInfo/models/User.cs
+++ backend/DataBaseTrello/DataBaseInfo/models/User.cs
@@ -11,9 +11,12 @@ namespace DataBaseInfo.models
     public class User
     {
         public int Id { get; set; }
-        public string UserName { get; set; } = string.Empty;
-        public string UserEmail { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string SecondName { get; set; } = string.Empty;
         public string UserPassword { get; set; } = string.Empty;
+        public Guid InviteId { get; set; } = Guid.Empty;
+        public string Sex { get; set; } = string.Empty;
+        public string Avatar {  get; set; } = string.Empty;
         public virtual List<ProjectUser> ProjectUsers { get; set; } = new();
         public virtual RefreshToken? RefreshToken { get; set; } 
     }
