30ff29b488a858019c566bb30c003db41dbaca74
INSANESX <galan.va@mail.ru>
2025-08-16 15:51:19 +0300

    homepage_0.11

--- backend/DataBaseTrello/DataBaseInfo/obj/Debug/net8.0/DataBaseInfo.AssemblyInfo.cs
+++ backend/DataBaseTrello/DataBaseInfo/obj/Debug/net8.0/DataBaseInfo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataBaseInfo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cbfea9fd061020c56110791611a4934f92b9b6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43b4db7d04cbfe03553fd3a3c37f19a03146e3d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataBaseInfo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataBaseInfo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/react-vite-intro/src/components/Login/Title.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "./Title.module.css";
-
-const Title = ({ text }) => <h1 className={styles.title}>{text}</h1>;
-
-export default Title;
