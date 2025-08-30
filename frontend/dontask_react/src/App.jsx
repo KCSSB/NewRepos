744a7fc428a988b470e2b606965208a8659cbea4
INSANESX <galan.va@mail.ru>
2025-08-31 01:14:34 +0300

    Update App.jsx

--- frontend/dontask_react/src/App.jsx
+++ frontend/dontask_react/src/App.jsx
@@ -6,14 +6,9 @@ import HomePage from "./pages/Home/Home";
 import TaskPage from "./pages/Task/Task";
 import SettingsPage from "./pages/Settings/Settings";
 import { ToastProvider } from "./components/Toast/ToastContext";
-import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   const navigate = useNavigate();
-
-  useEffect(() => {
-    localStorage.removeItem("token");
-  }, []);
 
   useEffect(() => {
     const handleStorageChange = (e) => {
@@ -30,16 +25,13 @@ function App() {
 
   return (
     <ToastProvider>
-      <AuthProvider>
-        {" "}
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/auth/*" element={<AuthPage />} />
-          <Route path="/home" element={<HomePage />} />
-          <Route path="/task" element={<TaskPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-        </Routes>
-      </AuthProvider>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/auth/*" element={<AuthPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/task" element={<TaskPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
     </ToastProvider>
   );
 }
