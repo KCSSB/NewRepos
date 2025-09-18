2a3d24572c6dc31aca2f199ad7344ce84beff74d
KCSSB <Nehbri.nehbri@yandex.ru>
2025-09-18 20:46:18 +0300

    IsWorked

---
+++ backend/DataBaseTrello/DataBaseInfo/Migrations/20250918115042_FixEntities.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataBaseInfo.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MemberResponsibleForCard",
+                table: "Tasks",
+                newName: "TaskResponsibleMembersц");
+
+            migrationBuilder.RenameColumn(
+                name: "DateStartWork",
+                table: "Tasks",
+                newName: "DateOfStartWork");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "TaskResponsibleMembersц",
+                table: "Tasks",
+                newName: "MemberResponsibleForCard");
+
+            migrationBuilder.RenameColumn(
+                name: "DateOfStartWork",
+                table: "Tasks",
+                newName: "DateStartWork");
+        }
+    }
+}
